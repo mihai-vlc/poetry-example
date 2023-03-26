4cc4516d7bc6795dc7c5c275aa68db808ba1eae0
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-03-26 17:48:37 +0300

    Updates the main file

--- src/my_api/main.py
+++ src/my_api/main.py
@@ -1,7 +1,10 @@
+from my_api.transform_string import transform
 
 
 def run():
     print("welcome to my app", __name__)
+    text = input("text = ")
+    print(transform(text))
 
 
 if __name__ == "__main__":
